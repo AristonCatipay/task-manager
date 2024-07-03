eb925214d127059fedfbd56e3b7d7c7be80dbb8d
AristonCatipay <aristoncatipay123@gmail.com>
2024-07-04 00:06:50 +0800

    Update the javascript code.

--- app.js
+++ app.js
@@ -4,30 +4,16 @@ const todoButton = document.querySelector(".todo-button"); // Button to add new 
 const todoList = document.querySelector(".todo-list"); // UL element to display tasks
 const filterOption = document.querySelector(".filter-todo"); // Dropdown for filtering tasks
 
-// Event Listeners
-todoButton.addEventListener("click", addTodo); // 3. Add new task when the button is clicked
-document.addEventListener("DOMContentLoaded", getTodos); // 5. Load tasks from localStorage when the page is loaded
-todoList.addEventListener("click", deleteOrCompleteTodo); // 8. Delete or complete a task
-filterOption.addEventListener("click", filterTodo); //10. Filter tasks based on completion status
+// Create event listeners for each button.
+todoButton.addEventListener("click", addTodo); // Step 2: Add new task when the button is clicked
+document.addEventListener("DOMContentLoaded", getTodos); // Step 5: Load tasks from localStorage when the page is loaded
+todoList.addEventListener("click", deleteOrCompleteTodo); // Step 7: Delete or complete a task
+filterOption.addEventListener("change", filterTodo); // Step 10: Filter tasks based on completion status
 
-// 1. Function to save tasks to localStorage
-function saveLocalTodos(todo) {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
-  }
-  todos.push(todo);
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-// 2.Function to add a new task
+// Step 1: Function to add a new task
 function addTodo(e) {
   // Prevent form submission
   e.preventDefault();
-
-  // Save the task to localStorage
   saveLocalTodos(todoInput.value);
 
   // Create a new todo div
@@ -57,9 +43,24 @@ function addTodo(e) {
   todoInput.value = "";
 }
 
-// 4. Function to load tasks from localStorage when the page is loaded
+// Step 3: Function to save tasks to localStorage
+// Middleware - a web server is middleware that connects websites to the backend database.
+// Metaphor: You should buy the ingredients first before you start to cook the dish.
+
+// The argument that is passed to the todo parameter is the actual textual data.
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// Step 4: Function to load tasks from localStorage when the page is loaded
 function getTodos() {
-  // Function to filter tasks based on completion status
   let todos;
   if (localStorage.getItem("todos") === null) {
     todos = [];
@@ -94,7 +95,31 @@ function getTodos() {
   });
 }
 
-// 6. Function to remove the task from the localStorage
+
+// Step 6: Function to delete or complete task
+function deleteOrCompleteTodo(e) {
+  const item = e.target;
+
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    removeLocalTodos(todo);
+
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  // If the complete button is clicked, toggle the completed class
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    // toggle() - Add if not existing, remove if existing.
+    todo.classList.toggle("completed");
+  }
+}
+
+// Step 8: Function to remove the task from the localStorage
+// The argument that will be passed to the todo parameter is the todoDiv container.
 function removeLocalTodos(todo) {
   let todos;
   if (localStorage.getItem("todos") === null) {
@@ -112,30 +137,7 @@ function removeLocalTodos(todo) {
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-// 7. Function to delete or complete task
-function deleteOrCompleteTodo(e) {
-  const item = e.target;
-
-  // If the delete button is clicked, remove the task from the list
-  if (item.classList[0] === "trash-btn") {
-    const todo = item.parentElement;
-    todo.classList.add("fall");
-    console.log(todo);
-    removeLocalTodos(todo);
-
-    todo.addEventListener("transitionend", (e) => {
-      todo.remove();
-    });
-  }
-
-  // If the complete button is clicked, toggle the completed class
-  if (item.classList[0] === "complete-btn") {
-    const todo = item.parentElement;
-    todo.classList.toggle("completed");
-  }
-}
-
-// 9. Function to filter task based on completion status
+// Step 9: Function to filter task based on completion status
 function filterTodo(e) {
   const todos = todoList.childNodes;
   todos.forEach(function (todo) {
@@ -156,7 +158,6 @@ function filterTodo(e) {
         } else {
           todo.style.display = "none";
         }
-        break;
     }
   });
 }
